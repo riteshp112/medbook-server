4141a024b7d8243fc6e0375c6c6944c5fa290a07
Ritesh Patel <ritesh.patel@daffodilsw.com>
2022-02-25 12:27:58 +0530

    added my profile

--- server.py
+++ server.py
@@ -36,7 +36,7 @@ def login():
     return Response(headers={'hua':'nahi'})
   else:
     print(res[0])
-    return Response(headers={'hua':'ha','details':jsonify({'name':res[0].name , 'username':res[0].username,'gender':res[0].gender,'dob':res[0].dob})})
+    return Response(headers={'hua':'ha','details':jsonify({'name':res[0]['name'] , 'username':res[0].username,'gender':res[0].gender,'dob':res[0].dob})})
   return ""
 @app.route("/home",methods=["GET","POST"])
 def home():
