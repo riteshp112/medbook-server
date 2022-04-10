a52ff6e35ef53b5ccd05aa686552fcb1512240af
Ritesh <ritesh.patel@daffodilsw.com>
2022-04-10 13:28:56 +0530

    nj

--- server.py
+++ server.py
@@ -3,56 +3,34 @@ from urllib import response
 from flask import Flask, jsonify, request ,redirect,Response
 import pymongo
 app=Flask(__name__)
-@app.route("/users",methods=['POST'])
-def users():
-  a=dict(request.json)
-  a=dict(a)
-  client = pymongo.MongoClient("mongodb+srv://riteshp112:6O8yYtaH1KvOaeyz@ritesh.l5gt1.mongodb.net/testdb?retryWrites=true&w=majority&authSource=admin")
-  db = client[ "testdb" ]
-  col = db[ "testcol" ]
-  x=col.insert_one(a)
-  return ""
-@app.route("/post",methods=['POST'])
-def post():
-  a=dict(request.json)
-  a=dict(a)
-  client = pymongo.MongoClient("mongodb+srv://riteshp112:6O8yYtaH1KvOaeyz@ritesh.l5gt1.mongodb.net/testdb?retryWrites=true&w=majority&authSource=admin")
-  db = client[ "testdb" ]
-  col = db[ "post" ]
-  x=col.insert_one(a)
-  return ""
-@app.route("/check",methods=['GET','POST'])
-def show():
-  return "Sever is running OK"
-@app.route("/login",methods=["GET","POST"])
-def login():
-  a=dict(request.json)
+@app.route("/invoke",methods=['GET','POST'])
+def invoke():
+  invokeRequest=dict(request.json)
   client = pymongo.MongoClient("mongodb+srv://riteshp112:6O8yYtaH1KvOaeyz@ritesh.l5gt1.mongodb.net/testdb?retryWrites=true&w=majority&authSource=admin",connect=False)
-  db = client[ "testdb" ]
-  col = db[ "testcol" ]
-  res=col.find(a)
-  res=list(res)
-  if len(res)==0:
-    return Response(headers={'hua':'nahi'})
-  else:
-    print(res[0])
-    return Response(headers={"hua":"ha","details":{"name":res[0]["name"] , "username":res[0]["username"],"gender":res[0]["gender"],"dob":res[0]["dob"]}})
-  return ""
-@app.route("/home",methods=["GET","POST"])
-def home():
-  a=dict(request.json)
-  client = pymongo.MongoClient("mongodb+srv://riteshp112:6O8yYtaH1KvOaeyz@ritesh.l5gt1.mongodb.net/testdb?retryWrites=true&w=majority&authSource=admin",connect=False)
-  db = client[ "testdb" ]
-  col = db[ "post" ]
-  res=col.find()
-  posts={}
-  i=0
-  a=int(a["plen"])
-  resf=res[:a+5]
-  for item in resf:
-    posts[str(i)]={"use":item["use"],"post":item["post"]}
-    i+=1
-  return Response(headers={"content":posts})
-  return ""
+  print(invokeRequest)
+  invokeType=invokeRequest['type']
+  if invokeType=='insert':
+    table=invokeRequest['table']
+    data=invokeRequest['data']
+    res=client['testdb'][table].insert_one(data)
+    return Response(headers={"output":res})
+  elif invokeType=='update':
+    table=invokeRequest['table']
+    id=invokeRequest['id']
+    changes=invokeRequest['changes']
+    myquery = { "_id": id }
+    newvalues = { "$set": changes }
+    res=client['testdb'][table].update_one(myquery, newvalues)    
+    return Response(headers={"output":res})
+  elif invokeType=='select':
+    print(str(request))
+    table=invokeRequest['table']
+    condition=invokeRequest['condition']
+    limit=int(invokeRequest['limit'])
+    data=client['testdb'][table].find(condition)
+    data=list(data)[:limit]
+    for item in data:
+      item['_id']=str(item['_id'])
+    return Response(headers={"output":data})
 if __name__ == '__main__':
   app.run(debug=True)
