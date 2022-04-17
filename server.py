1936ee3839e1fa55231c44da82306af3eccfefd4
Ritesh <ritesh.patel@daffodilsw.com>
2022-04-17 17:14:49 +0530

    updates

--- server.py
+++ server.py
@@ -3,38 +3,34 @@ from urllib import response
 from flask import Flask, jsonify, request ,redirect,Response
 import pymongo
 app=Flask(__name__)
-@app.route("/invoke",methods=["GET","POST"])
+@app.route("/invoke",methods=['GET','POST'])
 def invoke():
   invokeRequest=dict(request.json)
   client = pymongo.MongoClient("mongodb+srv://riteshp112:6O8yYtaH1KvOaeyz@ritesh.l5gt1.mongodb.net/testdb?retryWrites=true&w=majority&authSource=admin",connect=False)
   print(invokeRequest)
-  response.headers.add("Access-Control-Allow-Origin", "*")
-  response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
-  response.headers.add("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
-  invokeType=invokeRequest["type"]
-  if invokeType=="insert":
-    table=invokeRequest["table"]
-    data=invokeRequest["data"]
-    res=client["testdb"][table].insert_one(data)
-    response.headers.add("output",res)
-  elif invokeType=="update":
-    table=invokeRequest["table"]
-    id=invokeRequest["id"]
-    changes=invokeRequest["changes"]
+  invokeType=invokeRequest['type']
+  if invokeType=='insert':
+    table=invokeRequest['table']
+    data=invokeRequest['data']
+    res=client['testdb'][table].insert_one(data)
+    return Response(headers={"output":res,'Access-Control-Allow-Origin': '*','Access-Control-Allow-Headers':'Content-Type,Authorization','Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS'})
+  elif invokeType=='update':
+    table=invokeRequest['table']
+    id=invokeRequest['id']
+    changes=invokeRequest['changes']
     myquery = { "_id": id }
     newvalues = { "$set": changes }
-    res=client["testdb"][table].update_one(myquery, newvalues)    
-    response.headers.add("output",res)
-  elif invokeType=="select":
+    res=client['testdb'][table].update_one(myquery, newvalues)    
+    return Response(headers={"output":res,'Access-Control-Allow-Origin': '*','Access-Control-Allow-Headers':'Content-Type,Authorization','Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS'})
+  elif invokeType=='select':
     print(str(request))
-    table=invokeRequest["table"]
-    condition=invokeRequest["condition"]
-    limit=int(invokeRequest["limit"])
-    data=client["testdb"][table].find(condition)
+    table=invokeRequest['table']
+    condition=invokeRequest['condition']
+    limit=int(invokeRequest['limit'])
+    data=client['testdb'][table].find(condition)
     data=list(data)[:limit]
     for item in data:
-      item["_id"]=str(item["_id"])
-    response.headers.add("output",data)
-  return response
-if __name__ == "__main__":
+      item['_id']=str(item['_id'])
+    return Response(headers={"output":data,'Access-Control-Allow-Origin': '*','Access-Control-Allow-Headers':'Content-Type,Authorization','Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS'})
+if __name__ == '__main__':
   app.run(debug=True)
