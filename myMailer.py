cfab65a37785f42b8c19ca04fabf4783dd40d582
Ritesh <ritesh.patel@daffodilsw.com>
2022-10-01 01:02:05 +0530

    runtime

--- myMailer.py
+++ myMailer.py
@@ -15,19 +15,10 @@ api_instance = sib_api_v3_sdk.TransactionalEmailsApi(
 
 def MailSender(props):
     subject, sender, reply_to, html_content, to, params, headers, cc, bcc =props.values()
-    # subject = "from the Python SDK!"
-    # sender = {"name":"Ritesh Patel","email":"riteshp112@gmail.com"}
-    # reply_to = {"name":"Ritesh Patel","email":"riteshp112@gmail.com"}
-    # html_content = "<html><body><h1>This is my first transactional email </h1></body></html>"
-    # to = [{"email":"4bhis1@gmail.com","name":"Abhishek Kumar"}]
-    # params = {"parameter":"My param value","subject":"New Subject"}
     print(props,subject,sender,params,reply_to,to)
     send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(
         to=to,
-        # bcc=bcc,
-        # cc=cc,
         reply_to=reply_to,
-        # headers=headers,
         html_content=html_content,
         sender=sender,
         subject=subject,
