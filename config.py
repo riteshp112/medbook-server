4ce341cf27530a0ae70f3be5791d688ebf93c8e5
Ritesh <ritesh.patel@daffodilsw.com>
2022-11-28 17:47:44 +0530

    sort and limit

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
-from os import environ as env
-MAIL_API_KEY = env['MAIL_API_KEY'];
-MONGO_URI = env["MONGO_URI"];
+import os
+from dotenv import load_dotenv
+
+project_folder = os.path.expanduser("~/medbook/medbook-server")  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, ".env"))
+
+MAIL_API_KEY = os.getenv("MAIL_API_KEY")
+MONGO_URI = os.getenv("MONGO_URI")
 # MONGO_URI = 'mongodb://127.0.0.1/27017';
 MONGO_DATABASE = "testdb"
