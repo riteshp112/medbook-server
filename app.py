05044e90f1d6156d17a9c8518f2027272c4aff4d
Ritesh <ritesh.patel@daffodilsw.com>
2022-12-09 15:07:44 +0530

    sort and limit

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def sendMail():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- config.py
+++ config.py
@@ -6,6 +6,5 @@ load_dotenv(os.path.join(project_folder, ".env"))
 
 MAIL_API_KEY = os.getenv("MAIL_API_KEY")
 MONGO_URI = os.getenv("MONGO_URI")
-print(">>>>>>>>>", type(MONGO_URI))
 # MONGO_URI = 'mongodb://127.0.0.1/27017';
 MONGO_DATABASE = "testdb"
