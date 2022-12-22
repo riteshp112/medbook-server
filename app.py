80606a480d17bce64bab1a6db8428c363990d734
Ritesh <ritesh.patel@daffodilsw.com>
2022-12-22 15:15:27 +0530

    sort and limit

--- app.py
+++ app.py
@@ -13,15 +13,13 @@ CORS(app)
 def invoke():
     invokeRequest = {}
     try:
-        invokeRequest=json.loads(list(dict(request.args).keys())[0])
-        return OPERATIONS[invokeRequest["type"]](invokeRequest)
-    #     if request and request.args:
-    #         invokeRequest = dict(request.args)
-    #         invokeRequest = json.dumps(invokeRequest)
-    #         invokeRequest = json.loads(invokeRequest, object_hook=parseRequest)
-    #         return OPERATIONS[invokeRequest["type"]](invokeRequest)
-    #     else:
-    #         return "Server is running ok"
+        if request and request.json:
+            invokeRequest = dict(request.json)
+            invokeRequest = json.dumps(invokeRequest)
+            invokeRequest = json.loads(invokeRequest, object_hook=parseRequest)
+            return OPERATIONS[invokeRequest["type"]](invokeRequest)
+        else:
+            return "Server is running ok"
     except Exception as e:
         return {"response": {"error": str(e) + ": body " + str(invokeRequest)}}
 
