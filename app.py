2ebc2f7be6586ff883dfd310bb23efc979c52277
Ritesh <ritesh.patel@daffodilsw.com>
2023-09-24 22:46:16 +0530

    chnages

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import base64
 import json
-from flask import Flask, request
+from flask import Flask, Response, jsonify, request, send_file
 from flask_cors import CORS
 from myMailer import MailSender
 from index import OPERATIONS
 from utils import parseRequest
 from connections import db
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 CORS(app)
@@ -51,6 +53,56 @@ def sendGoodMorning():
     )
     res = MailSender(invokeRequest)
     return {"response": {"result": str(res)}}
+
+
+collection = db["images"]
+
+# Helper function to check if a file has an allowed extension
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/upload", methods=["POST"])
+def upload_image():
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    file = request.files["file"]
+
+    if file.filename == "":
+        return jsonify({"error": "No selected file"}), 400
+
+    if file and allowed_file(file.filename):
+        # Save the file to the upload folder
+        # Insert image metadata into MongoDB
+        file_data = base64.b64encode(file.read()).decode("utf-8")
+        file_insert = {"filename": file.filename, "data": file_data}
+        result = collection.insert_one(file_insert)
+
+        return (
+            jsonify(
+                {
+                    "message": "File uploaded successfully",
+                    "file_id": str(result.inserted_id),
+                }
+            ),
+            200,
+        )
+    else:
+        return jsonify({"error": "Invalid file type"}), 400
+
+
+@app.route("/retrieve/<file_id>", methods=["GET"])
+def retrieve_image(file_id):
+    file_data = collection.find_one({"_id": ObjectId(file_id)})
+
+    if not file_data:
+        return jsonify({"error": "File not found"}), 404
+    decoded_data = base64.b64decode(file_data['data'])
+    response = Response(decoded_data, content_type='application/octet-stream')
+    response.headers['Content-Disposition'] = f'attachment; filename={file_data["filename"]}'
+    return response
 
 
 if __name__ == "__main__":
