8c6b784ab6b9befff3b7ffe9ce6b943e74bb5849
Ritesh <ritesh.patel@daffodilsw.com>
2023-11-07 00:08:10 +0530

    chnages

--- app.py
+++ app.py
@@ -61,6 +61,7 @@ collection = db["images"]
 
 
 def allowed_file(filename):
+    return True
     ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
